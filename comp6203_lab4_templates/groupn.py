1bb32eb18de67c5fd6c5968ce51f4d78622c0190
Hossein Masjedi <120756379+xCamelot2001@users.noreply.github.com>
2024-11-28 18:01:41 +0000

    our company is positive now but others make no income

--- comp6203_lab4_templates/groupn.py
+++ comp6203_lab4_templates/groupn.py
@@ -38,7 +38,7 @@ class Companyn(TradingCompany):
 
         bids = []
         for trade in trades:
-            # Assign the vessel with the least idle time
+            # Assign the vessel with the least cost to the trade
             best_vessel = min(
                 self._fleet,
                 key=lambda vessel: self.calculate_cost(vessel, trade)
@@ -59,54 +59,107 @@ class Companyn(TradingCompany):
                     print(f"Competitor {company} won trades: {won_trades}")
 
         trades = [contract.trade for contract in contracts]
-        scheduling_proposal = self.find_contracts(trades)
-        self.apply_schedules(scheduling_proposal.schedules)
+        scheduling_proposal = self.find_schedules(trades)
+        self.current_schedule = scheduling_proposal.schedules
 
-    def find_contracts(self, trades):
-        """
-        Find the best contracts for the company.
-        """
+    # def find_contracts(self, trades):
+    #     """
+    #     Find the best contracts for the company.
+    #     """
+    #     schedules = {}
+    #     scheduled_trades = []
+    #     costs = {}
+
+
+    #     sorted_trades = sorted(
+    #         trades,
+    #         key=lambda trade: trade.earliest_pickup or float('inf')
+    #     )
+
+    #     for trade in sorted_trades:
+    #         best_vessel = None
+    #         best_schedule = None
+    #         min_cost = float("inf")
+
+    #         for vessel in self._fleet:
+    #             current_schedule = schedules.get(vessel, vessel.schedule)
+    #             new_schedule = current_schedule.copy()
+
+    #             # Try to add the trade to the schedule
+    #             new_schedule.add_transportation(trade)
+    #             if new_schedule.verify_schedule():
+    #                 total_cost = self.calculate_cost(vessel, trade)
+    #                 if total_cost < min_cost:
+    #                     best_vessel = vessel
+    #                     best_schedule = new_schedule
+    #                     min_cost = total_cost
+    #                     if best_vessel and best_schedule:
+    #                         schedules[best_vessel] = best_schedule
+    #                         scheduled_trades.append(trade)
+    #                         costs[trade] = min_cost
+
+    #     return ScheduleProposal(schedules, scheduled_trades, costs)
+
+    def propose_schedules(self, trades):
         schedules = {}
         scheduled_trades = []
-        costs = {}
-
-
-        sorted_trades = sorted(
-            trades,
-            key=lambda trade: trade.earliest_pickup or float('inf')
-        )
-
-        for trade in sorted_trades:
-            best_vessel = None
-            best_schedule = None
-            min_cost = float("inf")
-
-            for vessel in self._fleet:
-                current_schedule = schedules.get(vessel, vessel.schedule)
-                new_schedule = current_schedule.copy()
-
-                # Try to add the trade to the schedule
-                new_schedule.add_transportation(trade)
+        i = 0
+        while i < len(trades):
+            current_trade = trades[i]
+            competing_vessels = self.find_competing_vessels(current_trade)
+            if len(competing_vessels) == 0:
+                print(f"{current_trade.origin_port.name.split('-')[0]}"
+                      f" -> {current_trade.destination_port.name.split('-')[0]}: No competing vessels found")
+            for one_company in competing_vessels:
+                distance = self.headquarters.get_network_distance(
+                          competing_vessels[one_company].location, current_trade.origin_port)
+                print(f"{current_trade.origin_port.name.split('-')[0]}"
+                      f" -> {current_trade.destination_port.name.split('-')[0]}:"
+                      f" {one_company.name}'s {competing_vessels[one_company].name}"
+                      f" in {competing_vessels[one_company].location.name.split('-')[0]}"
+                      f" at {distance} NM")
+            is_assigned = False
+            j = 0
+            while j < len(self._fleet) and not is_assigned:
+                current_vessel = self._fleet[j]
+                current_vessel_schedule = schedules.get(current_vessel, current_vessel.schedule)
+                new_schedule = current_vessel_schedule.copy()
+                new_schedule.add_transportation(current_trade)
                 if new_schedule.verify_schedule():
-                    total_cost = self.calculate_cost(vessel, trade)
-                    if total_cost < min_cost:
-                        best_vessel = vessel
-                        best_schedule = new_schedule
-                        min_cost = total_cost
+                    schedules[current_vessel] = new_schedule
+                    scheduled_trades.append(current_trade)
+                    is_assigned = True
+                j += 1
+            i += 1
+        return ScheduleProposal(schedules, scheduled_trades, {})
 
-            if best_vessel and best_schedule:
-                schedules[best_vessel] = best_schedule
-                scheduled_trades.append(trade)
-                costs[trade] = min_cost
+    def find_schedules(self, trades):
+        schedules = {}
+        scheduled_trades = []
+        i = 0
+        while i < len(trades):
+            current_trade = trades[i]
+            is_assigned = False
+            j = 0
+            while j < len(self._fleet) and not is_assigned:
+                current_vessel = self._fleet[j]
+                current_vessel_schedule = schedules.get(current_vessel, current_vessel.schedule)
+                new_schedule = current_vessel_schedule.copy()
+                new_schedule.add_transportation(current_trade)
+                if new_schedule.verify_schedule():
+                    schedules[current_vessel] = new_schedule
+                    scheduled_trades.append(current_trade)
+                    is_assigned = True
+                j += 1
+            i += 1
+        return ScheduleProposal(schedules, scheduled_trades, {})
 
-        return ScheduleProposal(schedules, scheduled_trades, costs)
-
-    def apply_schedules(self, schedules):
-        """
-        Apply the schedules to the vessels.
-        """
-        for vessel, schedule in schedules.items():
-            vessel.schedule = schedule
+    # def apply_schedules(self, schedules):
+    #     """
+    #     Apply the schedules to the vessels.
+    #     """
+    #     for vessel, schedule in schedules.items():
+    #         vessel.schedule = schedule
 
     def create_bid(self, cost):
         """
